e26aa182b33d154a5f76f0eef16566b69fd39f08
yongqi8899 <1243994415@qq.com>
2024-07-28 22:13:49 +0200

    fix card

--- src/components/ImgCard.jsx
+++ src/components/ImgCard.jsx
@@ -9,7 +9,7 @@ export default function ImgCard({ src, alt, title = "" }) {
             e.target.src =
               "https://i0.wp.com/port2flavors.com/wp-content/uploads/2022/07/placeholder-614.png?fit=1200%2C800&ssl=1";
           }}
-          className="max-w-sm rounded-lg shadow-2xl w-100 h-100"
+          className="max-w-sm rounded-lg shadow-2xl w-100 h-80 object-cover"
         />
         {title && (
           <figcaption className="m-auto mt-2 text-center">{title}</figcaption>
