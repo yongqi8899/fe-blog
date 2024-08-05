4898da13d0bcbc1f8e9a3b2045413827a45ddd82
yongqi8899 <1243994415@qq.com>
2024-08-05 18:40:57 +0200

    add test CustomEvent

--- src/data/actions.js
+++ src/data/actions.js
@@ -1,5 +1,4 @@
 import { redirect } from "react-router-dom";
-import {toast} from 'react-toastify';
 
 export const createPost = async ({ request, res }) => {
   const formData = Object.fromEntries(await request.formData());
@@ -17,24 +16,32 @@ export const createPost = async ({ request, res }) => {
 export const updatePost = async ({ params, request }) => {
   const id = params.id;
   const formData = Object.fromEntries(await request.formData());
-  await fetch(`${import.meta.env.VITE_BASE_URL}/posts/${id}`, {
+  const res = await fetch(`${import.meta.env.VITE_BASE_URL}/posts/${id}`, {
     method: "PUT",
     headers: {
       "content-type": "application/json",
     },
     body: JSON.stringify(formData),
   });
+  if(!res.ok) throw Error("update Post is not successful")
   return redirect(`/posts/${id}`);
 };
 
 export const deletePost = async ({ params }) => {
   const id = params.id;
-  await fetch(`${import.meta.env.VITE_BASE_URL}/posts/${id}`, {
+  const res = await fetch(`${import.meta.env.VITE_BASE_URL}/posts/${id}`, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
     },
   });
+  if (!res.ok) {
+    const event = new CustomEvent('myToaster', { detail: { status: 'error', msg: 'Deletion failed!' } });
+    document.dispatchEvent(event);
+  } else {
+    const event = new CustomEvent('myToaster', { detail: { status: 'success', msg: 'Deletion worked!' } });
+    document.dispatchEvent(event);
+  }
   return redirect("/");
 };
 
--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,11 +1,26 @@
 import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
 import Header from "@/components/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 import { useLoaderData } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export default function RootLayout() {
-  const posts = useLoaderData()
+  const posts = useLoaderData();
+
+  useEffect(() => {
+    const handleEvent = (e) => {
+      if (e.detail.status === "success") {
+        toast.success(e.details.message);
+      } else {
+        toast.error(e.details.message);
+      }
+    };
+    document.addEventListener("myToaster", handleEvent);
+    return () => document.removeEventListener("myToaster", handleEvent);
+  }, []);
+
   return (
     <div>
       <Header />
